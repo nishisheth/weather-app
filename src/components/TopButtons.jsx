0a59f47c1f55745559ffa60332443f907aebf21e
Nishi Sheth <nishivsheth@gmail.com>
2023-12-04 16:27:51 +1100

    updated code for components and API integration

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function TopButtons() {
+function TopButtons(setQuery) {
     const cities = [
         {
             id: 1,
-            title: 'Sydney'
+            title: 'San francisco'
         },
         {
             id: 2,
@@ -22,13 +22,21 @@ function TopButtons() {
             id: 5,
             title: 'Tokyo'
         }
-    ]
-    return <div className="flex items-center justify-around my-6">
-        {cities.map((city) => (
-            <button key={city.id} className="text-white text-lo font-medium">{city.title}</button>
+    ];
 
-        ))}
-    </div>
+    return (
+        <div className="flex items-center justify-around my-6">
+            {cities.map((city) => (
+                <button
+                    key={city.id}
+                    className="text-white text-lg font-medium"
+                    onClick={() => setQuery({ q: city.title })}
+                >
+                    {city.title}
+                </button>
+            ))}
+        </div>
+    );
 }
 
 export default TopButtons
